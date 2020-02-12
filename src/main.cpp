23bcd21590ac9243bb71891f450dd3c813a8ca01
Arden Rasmussen <ardenisthebest@gmail.com>
2020-02-11 20:22:47 -0800

    Began division

--- src/main.cpp
+++ src/main.cpp
@@ -22,41 +22,7 @@
 #include "stb_image_write.h"
 
 using namespace glm;
-namespace std {
-template <class BidirIt, class Traits, class CharT, class UnaryFunction>
-std::basic_string<CharT>
-regex_replace(BidirIt first, BidirIt last,
-              const std::basic_regex<CharT, Traits> &re, UnaryFunction f) {
-  std::basic_string<CharT> s;
 
-  typename std::match_results<BidirIt>::difference_type positionOfLastMatch = 0;
-  auto endOfLastMatch = first;
-
-  auto callback = [&](const std::match_results<BidirIt> &match) {
-    auto positionOfThisMatch = match.position(0);
-    auto diff = positionOfThisMatch - positionOfLastMatch;
-
-    auto startOfThisMatch = endOfLastMatch;
-    std::advance(startOfThisMatch, diff);
-
-    s.append(endOfLastMatch, startOfThisMatch);
-    s.append(f(match));
-
-    auto lengthOfMatch = match.length(0);
-
-    positionOfLastMatch = positionOfThisMatch + lengthOfMatch;
-
-    endOfLastMatch = startOfThisMatch;
-    std::advance(endOfLastMatch, lengthOfMatch);
-  };
-
-  std::regex_iterator<BidirIt> begin(first, last, re), end;
-  std::for_each(begin, end, callback);
-
-  s.append(endOfLastMatch, last);
-
-  return s;
-}
 
 template <class Traits, class CharT, class UnaryFunction>
 std::string regex_replace(const std::string &s,
@@ -98,7 +64,7 @@ static Float epsilon_distance = 1e-3;
 static Float fov = M_PI / 2.0f;
 static uvec2 resolution = uvec2(500, 500);
 static std::size_t maximum_depth = 10;
-static std::size_t spp = 32;
+static std::size_t spp = 512;
 static std::vector<std::shared_ptr<Sdf>> objects;
 static Camera camera;
 
