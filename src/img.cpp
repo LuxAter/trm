0dcd3ca8090616b317d3bc06f82e1241537ac302
Arden Rasmussen <ardenisthebest@gmail.com>
2020-02-13 11:05:41 -0800

    Switched to C++17

--- src/img.cpp
+++ src/img.cpp
@@ -5,19 +5,25 @@
 
 #include "prof.hpp"
 
+bool ends_with(const std::string &str, const std::string &suffix) {
+  return str.size() >= suffix.size() &&
+         str.compare(str.size() - suffix.size(), std::string::npos, suffix) ==
+             0;
+}
+
 void write_file(const std::string &file_desc, const glm::uvec2 &res,
                 const uint8_t *raw) {
   PROF_FUNC("renderer", "file_desc", file_desc, "width", res.x, "height",
             res.y);
   stbi_flip_vertically_on_write(true);
-  if (file_desc.ends_with(".png")) {
+  if (ends_with(file_desc, "png")) {
     stbi_write_png(file_desc.c_str(), res.x, res.y, 3, raw,
                    res.x * 3 * sizeof(uint8_t));
-  } else if (file_desc.ends_with(".bmp")) {
+  } else if (ends_with(file_desc, ".bmp")) {
     stbi_write_bmp(file_desc.c_str(), res.x, res.y, 3, raw);
-  } else if (file_desc.ends_with(".tga")) {
+  } else if (ends_with(file_desc, ".tga")) {
     stbi_write_tga(file_desc.c_str(), res.x, res.y, 3, raw);
-  } else if (file_desc.ends_with(".jpg")) {
+  } else if (ends_with(file_desc, ".jpg")) {
     stbi_write_jpg(file_desc.c_str(), res.x, res.y, 3, raw, 75);
   }
 }
